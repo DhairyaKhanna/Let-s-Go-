9f27814e1dafdad6e51664a7bd9628a0e4d9a071
DhairyaKhanna <dhairya.khanna6668@gmail.com>
2019-03-27 04:11:06 -0800

    final commit

---
+++ app/src/main/java/com/let/letsgo/MainActivity.java
@@ -0,0 +1,18 @@
+package com.let.letsgo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        }
+
+
+}
+
